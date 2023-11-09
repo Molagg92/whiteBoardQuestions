33a448dcb12733f87dbc9f90fc448f29a81ab0b0
Erik Z <molagg92@gmail.com>
2023-11-09 13:18:53 -0800

    write function recursevly. start question 3

--- practiceQuestions.js
+++ practiceQuestions.js
@@ -2,14 +2,41 @@
 
 let userInput = "This is a URL";
 let URL = "";
-for (i = 0; i < userInput.length; i++ ){
-	let i = userInput[i];
-	if (i === " ") {
-	URL += "%20";
-	} else {	
-	URL += i;
-	}
+
+for (let i = 0; i < userInput.length; i++) {
+  let indx = userInput[i];
+  if (indx === " ") {
+    URL += "%20";
+  } else {
+    URL += indx;
+  }
 }
+
 console.log(URL);
 
 //========================================================================
+
+//with recursion!
+
+function recURL(userInput){
+  if (userInput.length === 0){
+    return "Please Enter a String!"
+    } else if (userInput[0] === " ") {
+      return"%20" + recURL(userInput.slice(1));
+    } else {
+      return userInput[0] + recURL(userInput.slice(1));
+    }
+  }
+  
+// ========================================================================
+
+//Question 2
+//without recursion
+
+let repeaterBeater = [1, 2, 3, "Monster!", 1, 2, 4, "Monster!"];
+let newArray = [];
+
+for (let i = 0; i < repeaterBeater.length; i++) {
+  let 
+
+}
