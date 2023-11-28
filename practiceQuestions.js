e8e86db75efe7d035b1abaa22f6b888c01060013
Erik Z <molagg92@gmail.com>
2023-11-28 04:55:53 -0800

    start question 5, need more reserch

--- practiceQuestions.js
+++ practiceQuestions.js
@@ -160,3 +160,25 @@
 
 //   return hasUniqueElements(remainingStr);
 // }
+
+//========================================================================
+//Question 5
+// I am going to spend some time reaserching each of the functions I am given to work with
+
+// ```
+// Write an algorithm that sorts an array without using the sort() method. There are many different sorting algorithms — take the time to read about the following:
+
+// Quick sort
+// Merge sort
+// Heap sort
+// Insertion sort
+// Bubble sort
+// Selection sort
+// You may implement any of the above algorithms (or your own) to solve the problem — as long as it doesn't use sort().
+
+// Example
+// Input: [9, 2, 7, 12]
+
+// Output: [2, 7, 9, 12]
+
+//```
